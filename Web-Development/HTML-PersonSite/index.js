d101d8a2248a88b057685a3ccc9e135f9f918da0
mrylraihan <mr.ylraihan@gmail.com>
2021-07-30 10:44:51 -0400

    Frontend refresher

---
+++ Web-Development/HTML-PersonSite/index.js
@@ -0,0 +1,15 @@
+const btn1 = document.getElementById("email")
+
+const showEmail = ()=> alert("mr.ylraihan@gmail.com");
+
+btn1.addEventListener("click", showEmail)
+
+
+const btn2 = document.getElementById("stuff")
+let a= 'this is the part about programming is having all these secrect messages every where'
+
+function sayHello (){
+    console.log(a)
+}
+
+btn2.addEventListener("click", sayHello);
